f55e4a9d9e2a1f7fc281b739ced6a868670af83b
carlos <carlos.melgarejo.solorzano@hotmail.com>
2023-10-06 18:10:02 -0500

    creacion del componente para la vista de edicion

---
+++ src/app/pages/edicion-cocktail/edicion-cocktail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edicion-cocktail',
+  templateUrl: './edicion-cocktail.component.html',
+  styleUrls: ['./edicion-cocktail.component.css']
+})
+export class EdicionCocktailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
