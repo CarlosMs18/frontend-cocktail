b69e6f08e3bb71d525b7f38d979275c694ba5b7b
carlos <carlos.melgarejo.solorzano@hotmail.com>
2023-10-06 16:46:48 -0500

    creando un compoenente compartido cabecera

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,12 @@ import {HttpClientModule} from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { ToastrModule } from 'ngx-toastr';
+import { SharedHeaderComponent } from './shared-header/shared-header.component';
 registerLocaleData(localeES, 'es');
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedHeaderComponent
   ],
   imports: [
     BrowserModule,
